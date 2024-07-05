57ac807b8b3978c5b059b014a3a4e0f88a031575
mhammed2008 <79751749+mhammed2008@users.noreply.github.com>
2024-07-05 12:45:36 +0300

    first commit

--- src/components/Teams/TeamHeader.vue
+++ src/components/Teams/TeamHeader.vue
@@ -13,7 +13,7 @@ let team = useTeamStore();
 
     <div>
       <h3 class="inline-flex items-center text-3xl relative">
-        <img src="./smiley.png" width="30" alt="" class="mr-2">
+        <img src="" width="30" alt="" class="mr-2">
         {{ team.name }} Team
         <div class="bg-green-400 w-5 h-5 text-xs text-white rounded-full flex justify-center items-center absolute -right-0 -top-2">
           {{ team.spots }}</div>
